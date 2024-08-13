8d1432d7d3a3fe72532ba6718b28c080d4de56f6
Riya Srivastava <riya.srivastava@w3villa.com>
2024-08-13 18:46:35 +0530

    Added validations.js

--- controller/userController.js
+++ controller/userController.js
@@ -8,9 +8,9 @@ const register = async(req,res)=>{
         return res.status(400).send("Please fill all the required fields");
     }
 
-    if(password !== confirmPassword){
-return res.status(400).send("Passwords do not match");
-    }
+//     if(password !== confirmPassword){
+// return res.status(400).send("Passwords do not match");
+//     }
     try{
         //Hashing the password with bcrypt: Here 10 is a salt rounds
         const hashedPassword = await bcrypt.hash(password,10);
@@ -20,8 +20,18 @@ return res.status(400).send("Passwords do not match");
         if(err){
              console.error("Error inserting data:",err);
              if(err.code === 'ER_DUP_ENTRY'){
+
+            //extrcting the error message to identify 
+                const errorMessage = err.sqlMessage;
+
+                //determine which field has duplicate entry
+                if(errorMessage.inludes('email')){
              return res.status(409).send('Email Already exists'); 
+             }else if(errorMessage.includes('mobileNumber')){
+                return res.status(409).send("Mobile Number already exists");
              }
+            }
+            //Other type of error has occurred
                 return res.status(500).send("An error ocurred while registering the user");
             }
         res.status(201).send("User Registered Successfully");
@@ -29,7 +39,7 @@ return res.status(400).send("Passwords do not match");
         });
     }catch (error) {
         console.error('Error hashing the password:', error);
-        res.status(500).send('AN error occurred during registeration.');
+        res.status(500).send('An error occurred during registeration.');
     }
     };
 
